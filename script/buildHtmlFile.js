148b223423a00ca6db01aab9331be0b14a1b2571
liuchengyuan <2267698673@qq.com>
2019-11-08 21:35:53 +0800

    生成目录

--- script/buildHtmlFile.js
+++ script/buildHtmlFile.js
@@ -1,23 +1,24 @@
 const path = require('path')
 const fs = require('fs')
 
+const baseUrl = 'https://github.com/chengyuan1216/chengyuan1216.github.io/blob/master/docs/'
 const rootDir = path.resolve(process.cwd(), 'docs')
-let parentTag = 'ul'
 let htmlStr = `<ul>`
-build(rootDir, parentTag)
+build(rootDir)
 htmlStr += `</ul>`
 
 function build(dir) {
   let dirChildList = fs.readdirSync(dir)
   dirChildList.forEach(child => {
     let isDir = fs.statSync(path.resolve(dir, child))
-    // let tag = parentTag === 'ul' ? 'li' : 'ul'
     if (isDir.isDirectory()) {
       htmlStr += `<li>${child}<ul>`
       build(path.resolve(dir, child))
       htmlStr += `</ul></li>`
     } else {
-      htmlStr += `<li>${child}</li>`
+      let relativeUrl = dir.split('\\docs\\')[1].replace(/\\/g, '/')
+      let url = baseUrl + '/' + relativeUrl + '/' + child
+      htmlStr += `<li><a href="${url}">${child}</a></li>`
     }
   })
 }
