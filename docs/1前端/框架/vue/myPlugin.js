4338a0a83f1cbbad1eaf2067bba2cdb0db0d9c8f
liuchengyuan <liucy@rp-pet.cn>
2020-01-07 15:05:57 +0800

    提交代码

---
+++ docs/1前端/框架/vue/myPlugin.js
@@ -0,0 +1,62 @@
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const path = require('path')
+const chalk = require('chalk')
+
+function Test() {
+
+}
+
+Test.prototype.apply = function(compiler) {
+  compiler.hooks.beforeCompile.tap('beforeCompile', (compilation) => {
+    console.log(compilation)
+  })
+}
+
+// 在package.json中加入下面代码
+/*
+  "vuePlugins": {
+    "service":["myPlugin"]
+  },
+*/
+
+
+module.exports = api => {
+    // 修改webpack配置
+    api.configureWebpack(webpackConfig => {
+        return {
+            mode:'development'
+        }
+    })
+    // 注册新的命令
+    api.registerCommand('play:build', args => {
+        console.log(chalk.green('build start.'))
+        debugger
+        let compiler = webpack({
+            mode: 'development', 
+            devtool: 'inline-source-map',
+            context: process.cwd(),
+            entry: {
+                app: [path.resolve(__dirname,'src/main.js')]
+            },
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: '[name].js',
+                chunkFilename: '[id].js'
+            },
+            plugins: [
+                new Test()
+            ]
+        })
+        compiler.run(err => {
+            if (!err) {
+                console.log(chalk.green('build success.'))
+            }
+        })
+    })
+}
+
+module.exports.defaultModes = {
+    'my-build': 'production'
+}
+
